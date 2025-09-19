643faafbb110abb0cf1c2159a8ea64ffd39c674d
Joshua Panasa <jumboaoa@gmail.com>
2025-09-19 00:13:25 -0500

    Ground Station Set up code
    Feel free to copy and mess around,
    
    - Window name changed to "Sugar Gliders Ground Station"

---
+++ Ground Station UI Code/groundstation_setup.py
@@ -0,0 +1,31 @@
+import sys
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QComboBox,
+    QDial,
+    QDoubleSpinBox,
+    QLabel,
+    QLineEdit,
+    QListWidget,
+    QMainWindow,
+    QSlider,
+    QSpinBox,
+)
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Sugar Gliders Ground Station")
+
+
+
+        
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+app.exec()
