0930b1fdd9735c94c5645a462ab9e422dcecadd9
Joshua Panasa <jumboaoa@gmail.com>
2025-09-23 16:34:13 -0500

    Reverted Code to base form - ready for programming
    Deleted all my testing stuff, feel free to start programming now.

--- Ground Station UI Code/groundstation_setup.py
+++ Ground Station UI Code/groundstation_setup.py
@@ -11,41 +11,10 @@ import sys
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowIcon(QIcon("Sugargliderstopicon"))
+        self.setWindowIcon(QIcon("Sugargliderstopicon.jpg"))
         self.setWindowTitle("Sugar Gliders Ground Station")
         layout = QGridLayout()
         self.setLayout(layout)
-
-        canvas = FigureCanvasQTAgg(fig)
-        layout.addWidget(canvas)
-
-x = [1, 2, 8, 3, 6]
-y= [9, 3, 1, 6, 3]
-
-fig = Figure(figsize=(4,4))
-ax = fig.add_subplot()
-ax.plot(x, y)
-
-       # self.label1 = QLabel("Test1")
-        # layout.addWidget(self.label1, 0, 0)
-
-        #self.label2 = QLabel("Test2")
-       # layout.addWidget(self.label2, 1, 0)
-
-       # self.input1 = QLineEdit()
-       # layout.addWidget(self.input1, 0, 1)
-
-      #  self.input2 = QLineEdit()
-       # layout.addWidget(self.input2, 1, 1)
-
-       # button = QPushButton("Submit")
-        #button.setFixedWidth(50)
-        #button.clicked.connect(self.display)
-       # layout.addWidget(button, 2, Qt.AlignmentFlag.AlignRight)
-
-    # def display(self):
-        # print(self.input1.text())
-        # print(self.input2.text())
 
 app = QApplication(sys.argv)
 window = MainWindow()
