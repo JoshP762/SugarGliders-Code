89c06d66f14167606ac91eec302f1a59ce79e9b8
Joshua Panasa <jumboaoa@gmail.com>
2025-09-24 20:27:23 -0500

    Added Documentation for PDR
    Added comments for PDR Presentation

--- Ground Station UI Code/groundstation_setup.py
+++ Ground Station UI Code/groundstation_setup.py
@@ -1,6 +1,7 @@
-from PyQt6.QtWidgets import (
+from PyQt6.QtWidgets import (                                  # PyQt6 imported libraries 
     QApplication, QWidget, QPushButton, QLabel, 
-    QLineEdit, QVBoxLayout, QGridLayout
+    QLineEdit, QVBoxLayout, QGridLayout, QStatusBar, 
+    QToolBar, 
 )
 from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtGui import QAction, QIcon
@@ -8,15 +9,17 @@ from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 import sys
 
-class MainWindow(QWidget):
+class MainWindow(QWidget):                                     # Window setup
     def __init__(self):
         super().__init__()
-        self.setWindowIcon(QIcon("Sugargliderstopicon.jpg"))
-        self.setWindowTitle("Sugar Gliders Ground Station")
-        layout = QGridLayout()
+        self.setWindowIcon(QIcon("Sugargliderstopicon.jpg"))   # Icon
+        self.setWindowTitle("Sugar Gliders Ground Station")    # Window Title
+        layout = QGridLayout()                                 # Grid layout to propagate later 
         self.setLayout(layout)
 
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
-sys.exit(app.exec())
+sys.exit(app.exec())                                           # Ensures program aborts correctly
+
+
