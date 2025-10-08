1edc99db23f750e5d4829d728dbcc2e79db042d8
Cosmo-Oneal <cosmo.oneal@gmail.com>
2025-10-07 20:36:14 -0500

    Update groundstation_setup.py

---
+++ Ground Station UI Code/groundstation_setup.py
@@ -0,0 +1,17 @@
+from PyQt5.QtWidgets import QDialog
+from ui_imagedialog import Ui_ImageDialog
+
+class ImageDialog(QDialog):
+    def __init__(self):
+        super(ImageDialog, self).__init__()
+
+        # Set up the user interface from Designer.
+        self.ui = Ui_ImageDialog()
+        self.ui.setupUi(self)
+
+        # Make some local modifications.
+        self.ui.colorDepthCombo.addItem("2 colors (1 bit per pixel)")
+
+        # Connect up the buttons.
+        self.ui.okButton.clicked.connect(self.accept)
+        self.ui.cancelButton.clicked.connect(self.reject)
